f95007f95cf2c51b7cf24ed715c32c8535e41651
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-21 16:49:36 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,45 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  createUser = data => {
+    console.log(data);
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.createUser({ name: this.state.name });
+  };
+
+  render() {
+    const { name, contacts } = this.state;
+    return (
+      <>
+        <h1>Phonebook</h1>
+
+        <ContactForm
+          createUser={this.createUser}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          name={name}
+          contacts={contacts}
+        />
+        <div>
+          <p>Contacts</p>
+          <ul>
+            <li>{}</li>
+          </ul>
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+export class ContactForm extends Component {
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="Name"></label>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          onChange={this.props.handleChange}
+          value={this.props.name}
+        />
+        <button htmlFor="Add">Add contact</button>
+      </form>
+    );
+  }
+}
