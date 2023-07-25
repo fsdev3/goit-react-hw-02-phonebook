4cfb2363a1289b7d90a1334296ffd89697dc357f
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-25 14:30:01 +0200

    upd

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -7,4 +7,15 @@ export const Container = styled.div`
   height: 520px;
   padding: 60px;
   border-radius: 4px;
+  h1 {
+    text-align: center;
+    font-size: 40px;
+    margin-bottom: 20px;
+  }
+  h2 {
+    text-align: center;
+    font-size: 30px;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
 `;
--- src/components/Contact/Contact.styled.js
+++ src/components/Contact/Contact.styled.js
@@ -3,4 +3,8 @@ import styled from 'styled-components';
 export const Item = styled.li`
   display: flex;
   justify-content: space-between;
+
+  button {
+    padding: 0 10px;
+  }
 `;
