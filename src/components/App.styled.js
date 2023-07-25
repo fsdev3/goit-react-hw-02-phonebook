b8d9a7ece86443063a5f60d54e7a3d0c975a69b6
Sergiy Nikolaev <122089422+fsdev3@users.noreply.github.com>
2023-07-25 18:46:15 +0200

    upd

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -2,22 +2,22 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   margin: 60px auto 0 auto;
-  background-color: #d8dcde;
+  background-color: #d8ecda;
   width: 360px;
   height: 100%;
-  padding: 60px;
+  padding: 80px;
   border-radius: 4px;
 
   h1 {
     text-align: center;
-    font-size: 40px;
+    font-size: 36px;
     margin-bottom: 20px;
   }
 
   h2 {
     text-align: center;
     font-size: 26px;
-    margin-top: 30px;
-    margin-bottom: 20px;
+    margin-top: 28px;
+    margin-bottom: 18px;
   }
 `;
